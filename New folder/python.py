724a4352a7aba6373ce219dfa903bc1cfe3041eb
amangorach <91610050+amangorach@users.noreply.github.com>
2022-11-12 17:26:59 +0530

    Add files via upload

---
+++ New folder/python.py
@@ -0,0 +1,13 @@
+# first code
+a = 33
+b = 33
+if b> a:
+    print("b is greater than a") 
+elif a == b:
+    print("a and b are equal")
+#second code
+def my_function (fname):
+  print(fname + " Refsnes")
+my_function("Emil") 
+my_function("Tobias") 
+my_function("Linus")
